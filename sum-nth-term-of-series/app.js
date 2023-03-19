1ca3a5625633586f1fa6e620e4b4e6937777429b
Raul <raulpena01998@gmail.com>
2023-03-18 23:58:10 -0500

    solved sum nth term of series problem

---
+++ sum-nth-term-of-series/app.js
@@ -0,0 +1,29 @@
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  if (n === 0) return 0.0;
+  let sum = 1;
+  let series = 4;
+  for (let i = 1; i < n; i++) {
+    sum += 1 / series;
+    series += 3;
+    console.log('hello');
+    console.log(sum);
+  }
+  return sum.toFixed(2);
+}
+SeriesSum(2);
