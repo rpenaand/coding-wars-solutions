d1f03715a6c5e9506e1d339790e4d338971bbc42
Raul <raulpena01998@gmail.com>
2023-04-03 23:53:56 -0500

    solved alternate capitilization problem

---
+++ alternate-capitalization/app.js
@@ -0,0 +1,27 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+// If you like this Kata, please try:
+
+// Indexed capitalization
+
+// Even-odd disparity
+
+function capitalize(s) {
+  const strArr = s.split('');
+  return [
+    strArr
+      .map((letter, idx) => (idx % 2 === 0 ? letter.toUpperCase() : letter))
+      .join(''),
+    strArr
+      .map((letter, idx) => (idx % 2 !== 0 ? letter.toUpperCase() : letter))
+      .join(''),
+  ];
+}
+
+capitalize('abc'); // -> [AbC, aBc]
