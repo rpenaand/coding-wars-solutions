653d32b1b605a698e7a75078fe2016f46be3863d
Raul <raulpena01998@gmail.com>
2023-04-15 00:03:21 -0500

    fixed bug in calculate average problem

--- calculate-average/app.js
+++ calculate-average/app.js
@@ -3,14 +3,15 @@
 // Note: Empty arrays should return 0.
 
 function findAverage(array) {
-  //Check if array is empty return 0 if empty
-  if (array.length < 0) return 0;
-
-  // loop through array and add values
-  //Divide sum by arr length to get average
-  //return average val
-  return array.reduce((acc, curVal) => (acc += curVal), 0) / array.length;
-}
+    //Check if array is empty return 0 if empty
+    if (array.length < 0) return 0;
+  
+    // loop through array and add values
+    const sum =  array.reduce((acc, curVal) => (acc + curVal), 0) 
+    //Divide sum by arr length to get average
+    //return average val
+    return sum / array.length
+  }
 
 const arr = [1, 2, 3];
 findAverage(arr); //-> 2
