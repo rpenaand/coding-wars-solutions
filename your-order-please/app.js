54fd1d67d1b95e3574ed905e372e2b9f7f56047d
Raul <raulpena01998@gmail.com>
2023-04-15 23:57:57 -0500

    added your order please problem

---
+++ your-order-please/app.js
@@ -0,0 +1,26 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words){
+    //seperate each word
+    const strArr = words.split("")
+
+    //Get num in each world
+    
+
+    //compare num in each word
+
+    //sort based on num
+
+    ///return string in order
+  }
+
+  order("the1 world3 good2") // -> the1 good2 world3
