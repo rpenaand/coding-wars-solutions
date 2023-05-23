3335b0812bdcbe99ecfc6b1ee8c16a3cb042c90d
Raul <raulpena01998@gmail.com>
2023-05-23 08:48:04 -0500

    added additional solution for switcheroo problem

--- switcheroo-anki/app.js
+++ switcheroo-anki/app.js
@@ -5,6 +5,28 @@
 // 'acb' --> 'bca'
 // 'aabacbaa' --> 'bbabcabb'
 
+//PREP
+
 function switcheroo(x) {
   return x.replace(/[ab]/g, (el) => (el === 'a' ? 'b' : 'a'));
 }
+
+function switcheroo(x) {
+  // Covert string into array
+  const strArr = x.split('');
+
+  //loop through array
+  //Check val of array
+  //if equal to a switch it to b
+  //if equal to b swtich it to a
+  const switchedArr = strArr.map((val) =>
+    val === 'a' ? 'b' : val === 'b' ? 'a' : 'c'
+  );
+
+  //Convert array back to string
+  //return new string
+  return switchedArr.join('');
+}
+
+switcheroo2('abc'); // -> bac
+switcheroo2('cbaa'); // -> cabb
